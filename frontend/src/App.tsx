db0683e6c21a63a324ef76c80f19c0e7d49b9480
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-07 15:19:50 -0300

    pasta copia git reiscaslureis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+                <div className="row">
+                    <div className="col-sm-6 px-3">
+                      <h5 className="text-secondary text-center">Taxa de Sucesso (%)</h5>
+                        <BarChart/>
+                    </div>
+                    <div className="col-sm-6 px-3">
+                      <h5 className="text-secondary text-center">Todas Vendas</h5>
+                        <DonutChart/>
+                    </div>
+                </div>
+
+                <div className="py-3">
+                    <h2 className="text-primary">Todas Vendas</h2>
+                </div>
+
+                <DataTable/>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
