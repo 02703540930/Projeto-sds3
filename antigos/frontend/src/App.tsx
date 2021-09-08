85b417689e3a0f1e4d3d1e78ac978bc7e365869c
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-08 18:00:52 -0300

    Security config

---
+++ antigos/frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+function App() {
+    return (
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+                <div className="row">
+                    <div className="col-sm-6 px-3">
+                      <h5 className="text-secondary text-center">Taxa de Sucesso (%)</h5>
+                        <BarChart/>
+                    </div>
+                    <div className="col-sm-6 px-3">
+                      <h5 className="text-secondary text-center">Todas Vendas</h5>
+                        <DonutChart/>
+                    </div>
+                </div>
+
+                <div className="py-3">
+                    <h2 className="text-primary">Todas Vendas</h2>
+                </div>
+
+                <DataTable/>
+                
+            </div>
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
