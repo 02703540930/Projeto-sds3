564208ad64eae0c2319ea37cc8192d9343b89f21
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-10 23:48:08 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,36 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts'
+import { SaleSum } from 'types/Sale';
+import { BASE_URL } from 'utils/request';
+
+type ChartData = {
+     labels: string[];
+     series: number[];
+}
 
 const DonutChart = () => {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    //FORMA ERRADA
+    let chartData : ChartData = { labels: [], series: []};
+
+    
+    //FORMA ERRADA
+    //requisicao para buscar soma das vendas dos vendedores
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSum[];                  //cria uma constante com os dados de SaleSum
+            const myLabels = data.map(x => x.sellerName);              //traz para myLabels cada xsellerName do datamap do SaleSum
+            const mySeries = data.map(x => x.sum);                     //traz para mySeries cada xsum do data.map do SaleSum 
+            
+            chartData = { labels: myLabels, series: mySeries };       //charData recebe os myLabels e mySeries criados no map acima
+            console.log(chartData)                                    //busca a resposta escrita no charData
+        });
+        
+
+
+    //const mockData = {
+    //   series: [477138, 499928, 444867, 220426, 473088],
+    //  labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
     
     const options = {
         legend: {
@@ -14,8 +40,8 @@ const DonutChart = () => {
     
     return (
         <Chart
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
