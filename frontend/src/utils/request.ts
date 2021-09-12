11fa46554727102cfdadf9879bba28eac3240e8e
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-11 23:56:52 -0300

    Env var

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+//?? operador de coalescencia nula que se não retornar do process a variavel de ambiente a esquerda(REACT_app_url) pega da direita (localhost) .
